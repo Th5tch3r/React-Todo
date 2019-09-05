74c6e5212a913f5f0778a813beaa64a7350b948b
Th5tch3r <amber.phan27@gmail.com>
2019-09-04 23:32:15 -0400

    Updates

---
+++ precache-manifest.e827a6527acc9da29a3cf9f6153256ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "251365c74ada538864e89ed5d3e503ba",
+    "url": "/React-Todo/index.html"
+  },
+  {
+    "revision": "dc03feb847d4835771ed",
+    "url": "/React-Todo/static/css/main.e330eadf.chunk.css"
+  },
+  {
+    "revision": "3837f3caaa6a6ec7c4b8",
+    "url": "/React-Todo/static/js/2.356972a7.chunk.js"
+  },
+  {
+    "revision": "dc03feb847d4835771ed",
+    "url": "/React-Todo/static/js/main.1c3290f7.chunk.js"
+  },
+  {
+    "revision": "8cccfb14f8793be292ed",
+    "url": "/React-Todo/static/js/runtime~main.58a39a8a.js"
+  }
+]);
